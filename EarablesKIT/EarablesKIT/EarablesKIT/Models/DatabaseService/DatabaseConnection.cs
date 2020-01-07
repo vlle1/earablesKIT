665ff1064c72f6883420e7f7cec0854aa7f4bc5d
Alligatooo <rittner.benni@t-online.de>
2020-01-07 19:04:53 +0100

    Beispiel Dokumentation von Methoden
    - Erstellen mit ///
    - XML

--- EarablesKIT/EarablesKIT/EarablesKIT/Models/DatabaseService/DatabaseConnection.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT/Models/DatabaseService/DatabaseConnection.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EarablesKIT.Models.DatabaseService
 {
-    class DatabaseConnection : IDataBaseConnection
+    internal class DatabaseConnection : IDataBaseConnection
     {
+        
         public void DeleteAllEntries()
         {
             throw new NotImplementedException();
         }
-
+        
         public void ExportTrainingsData(string path)
         {
             throw new NotImplementedException();
@@ -22,6 +22,12 @@ namespace EarablesKIT.Models.DatabaseService
             throw new NotImplementedException();
         }
 
+        //TODO Example Documentation
+        /// <summary>
+        /// Method <c>GetMostRecentEntriesAsync</c> connectes to the SQLite Database and gets the amount' recent entries. 
+        /// </summary>
+        /// <param name="amount">The 'amount' lastest database entries</param>
+        /// <returns>A Task countaining a List of database entries wrapped in class <see cref="DBEntry"/></returns>
         public Task<List<DBEntry>> GetMostRecentEntriesAsync(int amount)
         {
             throw new NotImplementedException();
@@ -30,12 +36,6 @@ namespace EarablesKIT.Models.DatabaseService
         public void SaveDBEntry(DBEntry entry)
         {
             throw new NotImplementedException();
-        }
-
-
-        public DatabaseConnection()
-        {
-             throw new NotImplementedException();
         }
     }
 }
