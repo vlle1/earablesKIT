9559db9ca4970f067e8759deb1855cc95a4ee6e8
David Höglinger <utzfk@student.kit.edu>
2020-01-10 12:23:10 +0100

    removed bin/ and obj/ and added testproject

---
+++ EarablesKIT/ViewModelTests/ImportExportViewModelTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ViewModelTests
+{
+    public class ImportExportViewModelTest
+    {
+        [Fact]
+        public void Import()
+        {
+            
+        }
+    }
+}
