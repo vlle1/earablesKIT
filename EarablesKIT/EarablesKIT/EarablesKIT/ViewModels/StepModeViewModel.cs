0c80654e5f899a119036186f9f5634dda8daeec8
Jan Ettrich <Jan.Ettrich00@gmx.de>
2020-01-13 16:15:27 +0100

    PropertyChanged Fnktionalität implementiert, Service Manager kommunikation skizziert

--- EarablesKIT/EarablesKIT/EarablesKIT/ViewModels/StepModeViewModel.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT/ViewModels/StepModeViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using EarablesKIT.Models;
+using EarablesKIT.Models.DatabaseService;
+using EarablesKIT.Models.Extentionmodel;
 using EarablesKIT.Models.Extentionmodel.Activities;
 using EarablesKIT.Models.Extentionmodel.Activities.RunningActivity;
 using EarablesKIT.Models.Extentionmodel.Activities.StepActivity;
@@ -6,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Xamarin.Forms;
 
@@ -18,20 +21,78 @@ namespace EarablesKIT.ViewModels
 		private Stopwatch timer;
 		private AbstractStepActivity stepActivity { get; set; }
 		private AbstractRunningActivity runningActivity { get; set; }
-		public string StepsDoneLastTime { get; set; }
-		public string DistanceWalkedLastTime { get; set; }
-		public string LastDataTime { get; set; }
-		public int StepCounter { get; set; }
-		public int DistanceWalked { get; set; }
-		public bool IsRunning { get; set; }
-		public double StepFrequency { get; set; }
-		
+		public string StepsDoneLastTime
+		{
+			get { return StepsDoneLastTime; }
+			set
+			{
+				StepsDoneLastTime = value;
+				OnPropertyChanged();
+			}
+		}
+		public string DistanceWalkedLastTime
+		{
+			get { return DistanceWalkedLastTime; }
+			set
+			{
+				DistanceWalkedLastTime = value;
+				OnPropertyChanged();
+			}
+		}
+		public string LastDataTime
+		{
+			get { return LastDataTime; }
+			set
+			{
+				LastDataTime = value;
+				OnPropertyChanged();
+			}
+		}
+		public int StepCounter
+		{
+			get { return StepCounter; }
+			set
+			{
+				StepCounter = value;
+				OnPropertyChanged();
+			}
+		}
+		public int DistanceWalked
+		{
+			get { return DistanceWalked; }
+			set
+			{
+				DistanceWalked = value;
+				OnPropertyChanged();
+			}
+		}
+		public bool IsRunning
+		{
+			get { return IsRunning; }
+			set
+			{
+				IsRunning = value;
+				OnPropertyChanged();
+			}
+		}
+		public double StepFrequency
+		{
+			get { return StepFrequency; }
+			set
+			{
+				StepFrequency = value;
+				OnPropertyChanged();
+			}
+		}
+
 
 		public StepModeViewModel()
 		{
 			StartActivityCommand = new Command(() => StartActivity());
 			StopActivityCommand = new Command(() => StopActivity());
 			IsRunning = false;
+			//var activityManager = ServiceManager.ServiceProvider.GetService(IActivityManager);
+			//IActivityManager activityManager = DependencyService.Get<IActivityManager>();
 			//stepActivity = ServiceManager.ServiceProvider.GetService(AbstractStepActivity);
 			//runningActivity = ServiceManager.ServiceProvider.GetService(AbstractRunningActivity);
 			UpdateLastData();
@@ -45,12 +106,12 @@ namespace EarablesKIT.ViewModels
 
 		public void OnRunningDone(object sender, ActivityArgs args)
 		{
-			IsRunning = true;
+			IsRunning = true; //
 		}
 
-		protected void OnPropertyChanged()
+		protected void OnPropertyChanged([CallerMemberName] string name = "")
 		{
-			throw new NotImplementedException();
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 		}
 
 		protected override void StartActivity()
@@ -70,6 +131,7 @@ namespace EarablesKIT.ViewModels
 
 		protected override void StopActivity()
 		{
+			timer.Stop();
 			stepActivity.ActivityDone -= OnActivityDone;
 			runningActivity.ActivityDone -= OnRunningDone;
 			IsRunning = false;
@@ -79,7 +141,11 @@ namespace EarablesKIT.ViewModels
 
 		private void UpdateLastData()
 		{
-			throw new NotImplementedException();
+			//IDataBaseConnection DatabaseService = ServiceManager.ServiceProvider.GetService<IDataBaseConnection();
+			//DBEntry entry = DatabaseService.getMostRecentEntriesAsync(1)[0];
+			//LastDataTime = entry.Date().ToString();
+			//StepsDoneLastTime = entry.TrainingsData[steps]
+			//DistanceWalkedLastTime = StepsDoneLastTime * Steplength
 		}
 
 
