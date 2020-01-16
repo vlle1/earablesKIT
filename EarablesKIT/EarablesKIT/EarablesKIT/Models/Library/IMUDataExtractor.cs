ccbe006783fcbcd0bdfe6ccf286b853ef9cb45e5
Erwin <e.mueller91718@gmail.com>
2020-01-16 18:20:28 +0100

    ExtractIMUDataString implementiert. Gyro werte werden richtig ausgelesen. Acc werte noch nicht ganz weil der Offset noch berücksichtigt werden muss darum schwankungen um schätzungsweise +-0,1

--- EarablesKIT/EarablesKIT/EarablesKIT/Models/Library/IMUDataExtractor.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT/Models/Library/IMUDataExtractor.cs
@@ -6,10 +6,61 @@ namespace EarablesKIT.Models.Library
 {
     class IMUDataExtractor
     {
-        public static IMUDataEntry ExtractIMUDataString(byte[] data, int accScaleFactor, double gyroScaleFactor)
+        public static IMUDataEntry ExtractIMUDataString(byte[] bytes, double accScaleFactor, double gyroScaleFactor, short accoffset)
         {
 
-            throw new NotImplementedException();
+            // Offest noch bearbeiten
+
+            // Initialise all required Objects
+            IMUDataEntry imuDataEntry = new IMUDataEntry();
+            Accelerometer accelerometer = new Accelerometer();
+            Gyroscope gyroscope = new Gyroscope();
+
+            // Get the Value from the Accelerometer
+            short AccXRaw = (short)(bytes[10] * 256 + bytes[11]);
+            short AccYRaw = (short)(bytes[12] * 256 + bytes[13]);
+            short AccZRaw = (short)(bytes[14] * 256 + bytes[14]);
+
+            // Scale the Accelerometervalues to G
+            float AccX = (float)(AccXRaw / accScaleFactor);
+            float AccY = (float)(AccYRaw / accScaleFactor);
+            float AccZ = (float)(AccZRaw / accScaleFactor);
+
+            // Set the Acceleration in G
+            accelerometer.G_X = AccX;
+            accelerometer.G_Y = AccY;
+            accelerometer.G_Z = AccZ;
+
+            // Calculate the Acceleration in m/s^2
+            AccX = AccX * 9.80665f;
+            AccY = AccY * 9.80665f;
+            AccZ = AccZ * 9.80665f;
+
+            // Set the Acceleration in m/s^2
+            accelerometer.MperS_X = AccX;
+            accelerometer.MperS_Y = AccY;
+            accelerometer.MperS_Z = AccZ;
+
+            // Get the Value from the Gyroscope
+            short GyroXRaw = (short)(bytes[4] * 256 + bytes[5]);
+            short GyroYRaw = (short)(bytes[6] * 256 + bytes[7]);
+            short GyroZRaw = (short)(bytes[8] * 256 + bytes[9]);
+
+            // Scale the Gyroscopevalues to deg/s
+            float GyroX = (float)(GyroXRaw / gyroScaleFactor);
+            float GyroY = (float)(GyroYRaw / gyroScaleFactor);
+            float GyroZ = (float)(GyroZRaw / gyroScaleFactor);
+
+            // Set the Gyrochanges in deg/s
+            gyroscope.DegsPerSec_X = GyroX;
+            gyroscope.DegsPerSec_X = GyroY;
+            gyroscope.DegsPerSec_X = GyroZ;
+
+            // Set the Data in the IMUDataEntry
+            imuDataEntry.Acc = accelerometer;
+            imuDataEntry.Gyro = gyroscope;
+
+            return imuDataEntry;
         }
 
         public static int ExtractIMURangeAccelerometer(byte[] bytes)
