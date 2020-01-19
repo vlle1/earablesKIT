42dae7ad9e4ea02c6da285c1f2b16b0848a4bb7f
ichbinstudent <utzfk@student.kit.edu>
2020-01-19 13:14:44 +0100

    added empty classes

---
+++ EarablesKIT/EarablesKIT/EarablesKIT/ViewModels/DebugViewModel.cs
@@ -0,0 +1,19 @@
+﻿using EarablesKIT.Models.DatabaseService;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EarablesKIT.ViewModels
+{
+    class DebugViewModel
+    {
+
+        public ObservableCollection<DBEntry> TrainingsData { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public DebugViewModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
