a1eb975f3ff0da46f1d318ace76ca82edd032ef1
Jan Ettrich <Jan.Ettrich00@gmx.de>
2020-01-30 13:08:31 +0100

    Kommentare in Views hinzugefügt

--- EarablesKIT/EarablesKIT/EarablesKIT/Views/ListenAndPerformActivePage.xaml.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT/Views/ListenAndPerformActivePage.xaml.cs
@@ -1,29 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EarablesKIT.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace EarablesKIT.Views
 {
+	/// <summary>
+	/// Codebehind class of ListenAndPerformActivePage.
+	/// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListenAndPerformActivePage : ContentPage
 	{
+		/// <summary>
+		/// ViewModel that will be the Binding Context of this class.
+		/// </summary>
 		private ListenAndPerformViewModel ViewModel { get; set; }
+
+		/// <summary>
+		/// Initializes the ViewModel.
+		/// </summary>
+		/// <param name="lapvm">Binding Context of the passive page</param>
 		public ListenAndPerformActivePage(ListenAndPerformViewModel lapvm)
 		{
 			ViewModel = lapvm;
 			InitializeComponent();
 		}
+
+		/// <summary>
+		/// Bound to the Clicked event of the Stop Button. Delegates to the ViewModel and changes the view to passive.
+		/// </summary>
+		/// <param name="sender">The sender of the event</param>
+		/// <param name="args">Ignored</param>
 		public void OnStopButtonClicked(object sender, EventArgs args)
 		{
 			ViewModel.StopActivity();
 			ChangeView();
 		}
 
+		/// <summary>
+		/// Changes the view to passive.
+		/// </summary>
 		public async void ChangeView()
 		{
 			await Navigation.PopModalAsync();
--- EarablesKIT/EarablesKIT/EarablesKIT/Views/StepModeActivePage.xaml.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT/Views/StepModeActivePage.xaml.cs
@@ -1,28 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EarablesKIT.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace EarablesKIT.Views
 {
+	/// <summary>
+	/// Codebehind class of the StepModeActivePage.
+	/// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class StepModeActivePage : ContentPage
 	{
+		/// <summary>
+		/// ViewModel that will be the Binding Context of this class.
+		/// </summary>
 		private StepModeViewModel ViewModel { get; set; }
+
+		/// <summary>
+		/// Initializes the ViewModel.
+		/// </summary>
+		/// <param name="smvm">Binding Context of the passive page</param>
 		public StepModeActivePage(StepModeViewModel smvm)
 		{
 			InitializeComponent();
 			ViewModel = smvm;
 		}
+
+		/// <summary>
+		/// Bound to the Clicked event of the Stop Button. Delegates to the ViewModel and changes the view to passive.
+		/// </summary>
+		/// <param name="sender">The sender of the event</param>
+		/// <param name="args">Ignored</param>
 		public void OnStopButtonClicked(object sender, EventArgs args)
 		{
 			ViewModel.StopActivity();
 			ChangeView();
 		}
+
+		/// <summary>
+		/// Changes the view to passive.
+		/// </summary>
 		public async void ChangeView()
 		{
 			await Navigation.PopModalAsync();
