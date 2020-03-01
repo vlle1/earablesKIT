2688edfbc1776c0d0c4b2b880d1fdc1aa2149de6
Alligatooo <rittner.benni@t-online.de>
2020-03-01 16:45:10 +0100

    Splashscreen

--- EarablesKIT/EarablesKIT/EarablesKIT.Android/MainActivity.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Rg.Plugins.Popup.Services;
 
 namespace EarablesKIT.Droid
 {
-    [Activity(Label = "EarablesKIT", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "eSense Fitness", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ EarablesKIT/EarablesKIT/EarablesKIT.Android/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EarablesKIT.Droid
+{
+    [Activity(Label= "eSense Fitness", Theme = "@style/Theme.Splash", Icon = "@mipmap/icon", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.SplashLayout);
+            System.Threading.ThreadPool.QueueUserWorkItem(o => LoadActivity());
+        }
+
+        private void LoadActivity()
+        {
+            System.Threading.Thread.Sleep(2000); // Simulate a long pause    
+            RunOnUiThread(() => StartActivity(typeof(MainActivity)));
+        }
+    }
+}
