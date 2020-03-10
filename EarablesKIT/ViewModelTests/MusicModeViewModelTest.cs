526a7dd16a8767a9dfb7658a0a77dc718d40f63d
ichbinstudent <utzfk@student.kit.edu>
2020-03-10 15:01:45 +0100

    MusicModeViewModelTest hinzugefügt.

---
+++ EarablesKIT/ViewModelTests/MusicModeViewModelTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ViewModelTests
+{
+    public class MusicModeViewModelTest
+    {
+        [Fact]
+        public void test1()
+        {
+
+        }
+
+    }
+}
