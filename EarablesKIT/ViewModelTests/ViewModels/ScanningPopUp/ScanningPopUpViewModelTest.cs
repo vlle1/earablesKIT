e58cdd76bc7366526bbc9a1c7a1c14b41187ebe9
Alligatooo <rittner.benni@t-online.de>
2020-03-16 22:30:54 +0100

    Some more tests

--- EarablesKIT/ViewModelTests/ViewModels/ScanningPopUp/ScanningPopUpViewModelTest.cs
+++ EarablesKIT/ViewModelTests/ViewModels/ScanningPopUp/ScanningPopUpViewModelTest.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Security;
 using System.Text;
+using System.Threading.Tasks;
 using EarablesKIT.Models;
+using EarablesKIT.Models.DatabaseService;
 using EarablesKIT.Models.Library;
 using EarablesKIT.ViewModels;
+using EarablesKIT.Views;
 using Moq;
 using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.Exceptions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Rg.Plugins.Popup.Contracts;
+using Rg.Plugins.Popup.Services;
 using Xamarin.Forms;
 using Xunit;
+
 
 namespace ViewModelTests.ViewModels.ScanningPopUp
 {
@@ -63,7 +75,8 @@ namespace ViewModelTests.ViewModels.ScanningPopUp
 
 
         [Fact]
-        public void testOnDeviceConnectionStateChanged()
+        
+        public void testNewDeviceFound()
         {
             //Für den ServiceProviderMock
             //Muss enthalten sein, damit der Mock nicht überschrieben wird
@@ -103,5 +116,233 @@ namespace ViewModelTests.ViewModels.ScanningPopUp
             Assert.Equal(0, toTest.DevicesList.IndexOf(mockSecondIDevice.Object)); ;
             Assert.Equal(1, toTest.DevicesList.IndexOf(mockIDevice.Object)); ;
         }
+
+        [Fact]
+        public void testConnectCommand()
+        {
+            IServiceProvider unused = ServiceManager.ServiceProvider;
+
+            FieldInfo instanceToMock = typeof(ServiceManager).GetField("_serviceProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            Mock<IServiceProvider> providerMock = new Mock<IServiceProvider>();
+            Mock<IEarablesConnection> earablesConnectionMock = new Mock<IEarablesConnection>();
+            Mock<IDevice> deviceMock = new Mock<IDevice>();
+            Mock<IExceptionHandler> exceptionHandlerMock = new Mock<IExceptionHandler>();
+
+            earablesConnectionMock.Setup(x => x.ConnectToDevice(deviceMock.Object));
+            earablesConnectionMock.SetupAdd(x => x.NewDeviceFound += ((sender, args) => { })).Verifiable();
+            exceptionHandlerMock.Setup(x => x.HandleException(It.IsAny<Exception>())).Verifiable();
+
+            providerMock.Setup(x => x.GetService(typeof(IEarablesConnection))).Returns(earablesConnectionMock.Object).Verifiable();
+            providerMock.Setup(x => x.GetService(typeof(IExceptionHandler))).Returns(exceptionHandlerMock.Object).Verifiable();
+
+            instanceToMock.SetValue(null, providerMock.Object);
+
+
+            //Testing
+            ScanningPopUpViewModel viewModelToTest = new ScanningPopUpViewModel();
+            viewModelToTest.ConnectDeviceCommand.Execute(deviceMock.Object);
+
+            earablesConnectionMock.Verify(x => x.ConnectToDevice(deviceMock.Object), Times.Once());
+            exceptionHandlerMock.Verify(x => x.HandleException(It.IsAny<Exception>()), Times.Never);
+
+            providerMock.VerifyAll();
+
+        }
+
+        [Fact]
+        public void testConnectCommandException()
+        {
+            IServiceProvider unused = ServiceManager.ServiceProvider;
+
+            FieldInfo instanceToMock = typeof(ServiceManager).GetField("_serviceProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            Mock<IServiceProvider> providerMock = new Mock<IServiceProvider>();
+            Mock<IEarablesConnection> earablesConnectionMock = new Mock<IEarablesConnection>();
+            Mock<IDevice> deviceMock = new Mock<IDevice>();
+            Mock<IExceptionHandler> exceptionHandlerMock = new Mock<IExceptionHandler>();
+
+            earablesConnectionMock.Setup(x => x.ConnectToDevice(deviceMock.Object)).Throws(new DeviceConnectionException(new Guid(), null,null));
+
+            exceptionHandlerMock.Setup(x => x.HandleException(It.IsAny<DeviceConnectionException>()));
+
+            providerMock.Setup(x => x.GetService(typeof(IEarablesConnection))).Returns(earablesConnectionMock.Object).Verifiable();
+            providerMock.Setup(x => x.GetService(typeof(IExceptionHandler))).Returns(exceptionHandlerMock.Object).Verifiable();
+
+            instanceToMock.SetValue(null, providerMock.Object);
+
+
+            //Testing
+            ScanningPopUpViewModel viewModelToTest = new ScanningPopUpViewModel();
+            viewModelToTest.ConnectDeviceCommand.Execute(deviceMock.Object);
+
+            //Asserts
+            Assert.Empty(viewModelToTest.DevicesList);
+            earablesConnectionMock.Verify(x => x.ConnectToDevice(deviceMock.Object), Times.Once());
+            exceptionHandlerMock.Verify(x => x.HandleException(It.IsAny<Exception>()), Times.Once);
+            providerMock.VerifyAll();
+
+
+        }
+
+        [Fact]
+        public void testScanningCommand()
+        {
+            IServiceProvider unused = ServiceManager.ServiceProvider;
+
+            FieldInfo instanceToMock = typeof(ServiceManager).GetField("_serviceProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            Mock<IServiceProvider> providerMock = new Mock<IServiceProvider>();
+            Mock<IEarablesConnection> earablesConnectionMock = new Mock<IEarablesConnection>();
+            Mock<IExceptionHandler> exceptionHandlerMock = new Mock<IExceptionHandler>();
+
+
+            earablesConnectionMock.Setup(x => x.StartScanning());
+            earablesConnectionMock.Setup(x => x.IsBluetoothActive).Returns(true);
+            
+
+            providerMock.Setup(x => x.GetService(typeof(IEarablesConnection))).Returns(earablesConnectionMock.Object);
+            providerMock.Setup(x => x.GetService(typeof(IExceptionHandler))).Returns(exceptionHandlerMock.Object);
+            
+            instanceToMock.SetValue(null, providerMock.Object);
+
+            FieldInfo currentCrossPermissionMock =
+                typeof(CrossPermissions).GetField("implementation", BindingFlags.NonPublic | BindingFlags.Static);
+            
+            Mock<IPermissions> mockImplementation = new Mock<IPermissions>();
+            Mock<IPermissions> valueMock = new Mock<IPermissions>();
+
+            mockImplementation.Setup(x => x.CheckPermissionStatusAsync(Permission.Location)).Returns(Task.FromResult(PermissionStatus.Granted));
+
+            currentCrossPermissionMock.SetValue(null, new Lazy<IPermissions>(() => mockImplementation.Object));
+
+
+            //Testing
+            ScanningPopUpViewModel viewModelToTest = new ScanningPopUpViewModel();
+            viewModelToTest.ScanDevicesCommand.Execute(null);
+
+            providerMock.VerifyAll();
+            earablesConnectionMock.VerifyAll();
+            mockImplementation.VerifyAll();
+            valueMock.VerifyAll();
+        }
+
+        [Fact]
+        public void testScanningCommandWithoutPermissions()
+        {
+            IServiceProvider unused = ServiceManager.ServiceProvider;
+
+            FieldInfo instanceToMock = typeof(ServiceManager).GetField("_serviceProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+
+            //Servii
+            Mock<IServiceProvider> providerMock = new Mock<IServiceProvider>();
+            Mock<IEarablesConnection> earablesConnectionMock = new Mock<IEarablesConnection>();
+            Mock<IExceptionHandler> exceptionHandlerMock = new Mock<IExceptionHandler>();
+
+            //earablesConnectionMock.Setup(x => x.StartScanning());
+            earablesConnectionMock.Setup(x => x.IsBluetoothActive).Returns(false);
+
+            providerMock.Setup(x => x.GetService(typeof(IEarablesConnection))).Returns(earablesConnectionMock.Object);
+            providerMock.Setup(x => x.GetService(typeof(IExceptionHandler))).Returns(exceptionHandlerMock.Object);
+            instanceToMock.SetValue(null, providerMock.Object);
+
+
+
+            //Crosspermission
+            FieldInfo currentCrossPermissionMock =
+                typeof(CrossPermissions).GetField("implementation", BindingFlags.NonPublic | BindingFlags.Static);
+
+            Mock<IPermissions> mockImplementation = new Mock<IPermissions>();
+            Mock<IPermissions> valueMock = new Mock<IPermissions>();
+
+            mockImplementation.Setup(x => x.CheckPermissionStatusAsync(Permission.Location)).Returns(Task.FromResult(PermissionStatus.Granted));
+            currentCrossPermissionMock.SetValue(null, new Lazy<IPermissions>(() => mockImplementation.Object));
+            
+
+            //Application
+            PropertyInfo propertyInfo = typeof(Application).GetProperty("Current", BindingFlags.Static);
+            Mock<Application> currentMock = new Mock<Application>();
+            Mock<Page> mainPageMock = new Mock<Page>();
+
+
+            currentMock.Setup(x => x.MainPage).Returns(mainPageMock.Object);
+            mainPageMock.Setup(x => x.DisplayAlert(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+            propertyInfo.SetValue(null, currentMock.Object);
+
+
+            //Testing
+            ScanningPopUpViewModel viewModelToTest = new ScanningPopUpViewModel();
+            viewModelToTest.ScanDevicesCommand.Execute(null);
+
+            providerMock.VerifyAll();
+            earablesConnectionMock.VerifyAll();
+            mockImplementation.VerifyAll();
+            valueMock.VerifyAll();
+        }
+
+        [Fact]
+        public void testCancelCommand()
+        {
+            IServiceProvider unused = ServiceManager.ServiceProvider;
+
+            FieldInfo instanceToMock = typeof(ServiceManager).GetField("_serviceProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            FieldInfo customField =
+                typeof(PopupNavigation).GetField("_customNavigation", BindingFlags.Static | BindingFlags.NonPublic);
+
+            Mock<IPopupNavigation> _customNavigationAsMock = new Mock<IPopupNavigation>();
+
+            _customNavigationAsMock.Setup(x => x.PopAsync(true));
+            customField.SetValue(null, _customNavigationAsMock.Object);
+
+            Mock<IServiceProvider> providerMock = new Mock<IServiceProvider>();
+
+            instanceToMock.SetValue(null, providerMock.Object);
+
+            Mock<IEarablesConnection> earablesConnectionMock = new Mock<IEarablesConnection>();
+            Mock<IExceptionHandler> exceptionHandlerMock = new Mock<IExceptionHandler>();
+            
+            providerMock.Setup(x => x.GetService(typeof(IEarablesConnection))).Returns(earablesConnectionMock.Object);
+            providerMock.Setup(x => x.GetService(typeof(IExceptionHandler))).Returns(exceptionHandlerMock.Object);
+
+            //Act 
+            ScanningPopUpViewModel toTestModel = new ScanningPopUpViewModel();
+            toTestModel.CancelCommand.Execute(null);
+
+            //Verify
+            providerMock.VerifyAll();
+            _customNavigationAsMock.VerifyAll();
+            earablesConnectionMock.VerifyAll();
+        }
+
+        [Fact]
+        public void testOnDeviceConnectionStateChanged()
+        {
+            FieldInfo customField =
+                typeof(PopupNavigation).GetField("_customNavigation", BindingFlags.Static | BindingFlags.NonPublic);
+
+            Mock<IPopupNavigation> _customNavigationAsMock = new Mock<IPopupNavigation>();
+            
+            _customNavigationAsMock.Setup(x => x.PopAsync(true));
+            customField.SetValue(null, _customNavigationAsMock.Object);
+
+            //Act 
+            ScanningPopUpViewModel.OnDeviceConnectionStateChanged(null, new DeviceEventArgs(true, ""));
+
+            //Verify
+            _customNavigationAsMock.VerifyAll();
+        }
+
+        [Fact]
+        public void testConnectedProperty()
+        {
+            ScanningPopUpViewModel.IsConnected = true;
+            Assert.True(ScanningPopUpViewModel.IsConnected);
+            ScanningPopUpViewModel.IsConnected = false;
+            Assert.False(ScanningPopUpViewModel.IsConnected);
+        }
+
+
     }
 }
