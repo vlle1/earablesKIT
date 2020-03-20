69745bd01c63126f35ff405ae90dd3e1834ce7e7
Jan Ettrich <Jan.Ettrich00@gmx.de>
2020-03-20 11:24:18 +0100

    Default Selected Activity

--- EarablesKIT/EarablesKIT/EarablesKIT/Views/CountModePage.xaml.cs
+++ EarablesKIT/EarablesKIT/EarablesKIT/Views/CountModePage.xaml.cs
@@ -24,6 +24,7 @@ namespace EarablesKIT.Views
             InitializeComponent();
 			ViewModel = new CountModeViewModel();
 			BindingContext = ViewModel;
+			ActivityView.SelectedItem = ViewModel.PossibleActivities[0];
         }
 
 		/// <summary>
